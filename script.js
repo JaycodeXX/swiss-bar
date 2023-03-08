9c730f81e5e60f2cba0caf829b55ecd99040c2e4
Onyeke Sunday Victor <diamondholdingslimited@gmail.com>
2023-03-08 18:00:13 +0100

    complete landing page

--- script.js
+++ script.js
@@ -6,9 +6,27 @@ menu.onclick = () =>{
     navbar.classList.toggle('active');
 }
 
+let section = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header .navbar a');
+
+
 window.onscroll = () =>{
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
+
+    section.forEach(sec =>{
+      let top = window.scrollY;
+      let height = sec.offsetHeight;
+      let offset = sec.offsetTop - 150;
+      let id = sec.getAttribute('id');
+
+      if(top => offset && top < offset + height){
+        navlinks.forEach(links =>{
+          links.classList.remove('active');
+          document.querySelector('header .navbar a[href*=' +id+']').classList.add('active');
+        });
+      };
+    });
 }
 document.querySelector('#search').onclick = () =>{
     document.querySelector('#search_form').classList.toggle('active');
@@ -30,4 +48,29 @@ var swiper = new Swiper(".home_slider", {
       clickable: true,
     },
   loop:true,
+  });
+
+
+var swiper = new Swiper(".review_slider", {
+    spaceBetween: 20,
+    centeredSlides: true,
+    autoplay: {
+      delay: 3500,
+      disableOnInteraction: false,
+    },
+  loop:true,
+  breakpoints: {
+    0:{
+      slidesPerView: 1,
+    },
+    640:{
+      slidesPerView: 2,
+    },
+    768:{
+      slidesPerView: 2,
+    },
+    1024:{
+      slidesPerView: 3,
+    },
+  },
   });
